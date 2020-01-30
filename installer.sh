d20b1908e7908a943ff09798f257d3e7f2ab8f71
scott96707 <scott96707@gmail.com>
2020-01-29 20:02:16 -0700

    Tweaked the installer. Reformated the code a bit to improve readability.

--- installer.sh
+++ installer.sh
@@ -20,18 +20,6 @@ trim_string(){
 	printf '%s\n' "$trim"
 }
 
-# Adds necessary repo to install VS Code
-setup_vscode() {
-    rpm --import https://packages.microsoft.com/keys/microsoft.asc
-
-	if [ -f /etc/yum.repos.d/vscode.repo ];
-	then
-		return 0
-	else
-		echo "$VS_CODE_REPO" > /etc/yum.repos.d/vscode.repo
-    fi
-} 
-
 pre_install() {
     log "Installing RPM Fusion"
     try dnf install -y \
@@ -45,9 +33,6 @@ pre_install() {
 	try dnf copr enable -y pschyska/alacritty
 	try dnf install -y alacritty
 
-    #log "VSCode - Adding Microsoft Repo"
-    #try setup_vscode
-    #dnf check-update
 }
 
 install_package() {
@@ -57,7 +42,7 @@ install_package() {
 			try go get -u "$1"
             ;;
 		*)
-		    log "Installing $1 - "
+		    log "Installing $1... "
 		    try dnf install -y "$1"
             ;;
     esac
@@ -75,45 +60,29 @@ $packages
 EOF
 }
 
-# Adding aliases and environment variable to the user's .bashrc
 source_variables() {
-    # Add for local user
-	if grep -lx "source ~/.config/aliases" $USER_HOME/.bashrc; 
-	then
-		return 0
-	else
-		echo "source ~/.config/aliases" >> $USER_HOME/.bashrc
-	fi
-	if grep -lx "source ~/.profile" $USER_HOME/.bashrc;
-	then
-		return 0
-	else
-		echo "source ~/.profile" >> $USER_HOME/.bashrc
-	fi
-    # Add for root user
-	if grep -lx "source $USER_HOME/.config/aliases" $HOME/.bashrc; 
-	then
-		return 0
-	else
-		echo "source $USER_HOME/.config/aliases" >> $HOME/.bashrc
-	fi
-	if grep -lx "source $USER_HOME/.profile" $HOME/.bashrc;
-	then
-		return 0
-	else
-		echo "source $USER_HOME/.profile" >> $HOME/.bashrc
-    fi
+    log "Adding source variables and aliases" 
+	if grep -lx "source ~/.config/aliases" $USER_HOME/.bashrc; then return 0;
+    else echo "source ~/.config/aliases" >> $USER_HOME/.bashrc;	fi
+	if grep -lx "source ~/.profile" $USER_HOME/.bashrc;	then return 0;
+	else echo "source ~/.profile" >> $USER_HOME/.bashrc; fi
+}
+
+setup_root() {
+    log "Adding root user links and settings"
+    cd $HOME && ln -s $USER_HOME/.vimrc
+	if grep -lx "source $USER_HOME/.config/aliases" $HOME/.bashrc; then return 0;
+	else echo "source $USER_HOME/.config/aliases" >> $HOME/.bashrc;	fi
+	if grep -lx "source $USER_HOME/.profile" $HOME/.bashrc; then return 0; 
+    else echo "source $USER_HOME/.profile" >> $HOME/.bashrc; fi
 }
 
 install_dots() {
 	log "Downloading dot files"
 	sudo -u $SUDO_USER git clone "$dotrepo" $USER_HOME/.config/rice > "$debug" || log "Dots have already been cloned"
 	log "Stowing dot files"
-	sudo -u $SUDO_USER bash -c '\
-        cd $HOME/.config/rice; 
-        stow --target="$HOME/" --ignore="gitignore" dots;'
-    log "Adding source variables and aliases" 
-    try source_variables
+	sudo -u $SUDO_USER bash -c 'cd $HOME/.config/rice; 
+        stow --target="$HOME/" --ignore="gitignore" dots;' > "$debug" || log "Dots failed to stow"
 }
 
 install_vimplug() {
@@ -123,9 +92,22 @@ install_vimplug() {
     ln -s $USER_HOME/.vim ~/
 }
 
-install_vscode_extensions() {
+setup_vscode() {
+    log "VSCode - Adding Microsoft Repo"
+    VS_CODE_REPO='[vscode]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF'
+	if [ -f /etc/yum.repos.d/vscode.repo ];	then return 0;
+	else echo "$VS_CODE_REPO" > /etc/yum.repos.d/vscode.repo; fi
+    
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
     try code --install-extension Shan.code-settings-sync
-}
+    dnf check-update
+} 
 
 cleanup() {
 	yon "Aborting rice"
@@ -135,22 +117,14 @@ main() {
 	dotrepo="https://github.com/scott96707/rice.git"
 	pkgfile="https://github.com/scott96707/rice/raw/master/packages"
 	USER_HOME="/home/$SUDO_USER"
-    VS_CODE_REPO='[vscode]
-name=Visual Studio Code
-baseurl=https://packages.microsoft.com/yumrepos/vscode
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.microsoft.com/keys/microsoft.asc
-EOF'
 	if [ "$RICE_DEBUG" == 1 ]; then debug="/dev/stdout"; else debug="/dev/null"; fi
-
 	trap cleanup INT
 
     pre_install
     install_packages
     install_dots
+    source_variables
     install_vimplug
-    #install_vscode_extensions
+    setup_root
 }
-
 main

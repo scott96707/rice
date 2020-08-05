a5b75d7ce7aeeef358be8099a3a60a229b91d1f6
scott96707 <scott96707@gmail.com>
2020-08-05 08:39:30 -0600

    Added pre-installer step for gcloud sdk repo setup

--- installer.sh
+++ installer.sh
@@ -33,6 +33,18 @@ pre_install() {
 	try dnf copr enable -y pschyska/alacritty
 	try dnf install -y alacritty
 
+    log "Adding yum/dnf with GCloud SDK repo"
+    sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
 }
 
 install_package() {
